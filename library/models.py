1916a49b95a068b80693258b034a7200a78d47c7
Redwa10 <111649804+Redwa10@users.noreply.github.com>
2025-02-25 23:56:51 +0300

    auto_now

--- library/models.py
+++ library/models.py
@@ -61,5 +61,5 @@ class Reservation(models.Model):
     ]
     member = models.foreignkey(Member, on_delete = models.CASCADE)
     book = models.OneToOneField(Books, on_delete= models.CASCADE)
-    reservation_date = models.DateField(auto_now_add= True)
+    reservation_date = models.DateField(auto_now= True)
     rstatus = models.CharField(max_length= 1, choices = reservation_status)
